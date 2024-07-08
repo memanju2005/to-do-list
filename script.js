30e1e771f9af97dd5c786679729d1e95fad334e0
me <@nothing.com>
2024-07-08 22:57:36 +0530

    added progress bar

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
 let pointer=0;
+let completes=0;
+let total=0;
+let pixel;
 const tasks=[];
+let progress=document.getElementById("complete");
 let list=document.getElementById("mylist");
 let congrat=document.getElementById("congrat");
 // for(i=0;i<tasks.length;i++)
@@ -16,9 +20,19 @@ function display_congrats(){
     congrat.style.display = "none"; // Hide the message after 3 seconds
   }, 1000);
 }
+function progress_setter(){
+  pixel=(completes/total)*100;
+  p=String(pixel);
+  p+="px"
+  progress.style.width=p;
+  console.log("done");
+  
+  // progress.style.width=pixel;
+}
 function submit() 
 {
-
+    total+=1;
+    progress_setter();
     text = document.getElementById("task").value;
     console.log(text);
     pointer+=1;
@@ -45,11 +59,15 @@ function submit()
             if (checkbox.checked) {
               display_congrats();
                 console.log("Checkbox is checked.");
+                completes+=1;
+                progress_setter();
                 li.style.textDecoration="line-through";
                 
 
               } else {
                 console.log("Checkbox is unchecked.");
+                completes-=1;
+                progress_setter();
                 li.style.textDecoration="none";
                 congrat.style.display="none";
               }
